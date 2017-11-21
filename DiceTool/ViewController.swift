4fb90f4c009f95e6f48dc82dd1bfc37448ad7fe7
WWK563388548 <wc563388548@gmail.com>
2017-11-21 13:05:07 +0800

    Initial Commit

---
+++ DiceTool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiceTool
+//
+//  Created by wwk on 2017/11/21.
+//  Copyright © 2017年 wwk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
