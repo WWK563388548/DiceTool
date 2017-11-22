4af8c582a7f0da19ef1af1d6f6cad640525a5f9c
WWK563388548 <wc563388548@gmail.com>
2017-11-22 21:29:01 +0800

    Optimized code of project, added function that can change dice pictures every time when restart app

--- DiceTool/ViewController.swift
+++ DiceTool/ViewController.swift
@@ -18,7 +18,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     @IBAction func rollDiceButton(_ sender: UIButton) {
-        
+        updateDiceImage()
+    }
+    
+    // This function will run on load time
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // This will change dice pictures every time when restart app
+        updateDiceImage()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Define a function to optimize code
+    func updateDiceImage() {
         // Generate random number when someone click the "Roll" button
         // arc4random_uniform() belongs to UIKit, eg. 若输入8, 就会生成0～7的随机数
         // Type casting
@@ -27,20 +44,9 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int(arc4random_uniform(UInt32(dicePicturesArray.count)))
         randomDiceIndex2 = Int(arc4random_uniform(UInt32(dicePicturesArray.count)))
         // 调用UIImageView的属性和方法, 使用UIImage()方法
-        diceImageView1.image = UIImage(named: dicePicturesArray[randomDiceIndex1]) // 使用按钮后会改变图片
+        diceImageView1.image = UIImage(named: dicePicturesArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: dicePicturesArray[randomDiceIndex2])
         
-    }
-    
-    // This function will run on load time
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
